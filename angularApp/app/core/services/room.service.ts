0bd5d3248c72d60b20bf5808a858b701b87950f0
ViktorBaklyshkin <icwee@icloud.com>
2018-04-11 22:34:48 +0300

    Добавил сервис комнат, сервис типов комнат, сделал добавление по кнопке Добавить в динамический список комнат

---
+++ angularApp/app/core/services/room.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Configuration } from '../../app.constants';
+import { Room } from '../../models/room';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class RoomService {
+
+  private headers: HttpHeaders;
+  private actionUrl: string;
+
+  constructor(private http: HttpClient, configuration: Configuration) {
+    this.actionUrl = configuration.Server + 'api/rooms/';
+
+    this.headers = new HttpHeaders();
+    this.headers = this.headers.set('Content-Type', 'application/json');
+    this.headers = this.headers.set('Accept', 'application/json');
+  }
+
+  getAll(): Observable<Room[]> {
+    console.log(this.actionUrl);
+    return this.http.get<Room[]>(this.actionUrl, { headers: this.headers });
+  }
+
+}
