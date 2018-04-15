5d4c0e822d629341356b8b68d53b83673c96f10a
ViktorBaklyshkin <icwee@icloud.com>
2018-04-15 22:44:21 +0300

    Изменил логику добавления комнат, теперь конаты добавляются по отдельности через апи в БД, изменена верстка навигационного блока

--- angularApp/app/core/services/project.service.ts
+++ angularApp/app/core/services/project.service.ts
@@ -1,31 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Configuration } from '../../app.constants';
-import { Room } from '../../models/room';
 import { Observable } from 'rxjs/Observable';
+import { Project } from '../../models/project';
+
 
 @Injectable()
-export class RoomService {
+export class ProjectService {
 
   private headers: HttpHeaders;
   private actionUrl: string;
 
   constructor(private http: HttpClient, configuration: Configuration) {
-    this.actionUrl = configuration.Server + 'api/rooms/';
+    this.actionUrl = configuration.Server + 'api/projects/';
 
     this.headers = new HttpHeaders();
     this.headers = this.headers.set('Content-Type', 'application/json');
     this.headers = this.headers.set('Accept', 'application/json');
   }
 
-  getAll(): Observable<Room[]> {
-    console.log(this.actionUrl);
-    return this.http.get<Room[]>(this.actionUrl, { headers: this.headers });
-  }
-
-  add(roomsToAdd: Room): Observable<Room> {
-    console.log(this.actionUrl);
-    return this.http.post<Room>(this.actionUrl, roomsToAdd, {headers: this.headers});
+  Add(project: Project): Observable<Project> {
+    console.log(project);
+    return this.http.post<Project>(this.actionUrl, project, { headers: this.headers });
   }
 
 }
