b8a1dac03ac9a67f5cdb480ad5caa6721a2ea98a
ViktorBaklyshkin <icwee@icloud.com>
2018-11-25 12:38:12 +0300

    Рефакторинг airexchange

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -108,14 +108,126 @@ namespace VentCalc.Controllers {
         [HttpGet]
         [Route("~/api/Projects/{id:int}/AirExchange")]
         public async Task<IActionResult> ReadAirExchange(int id) {
-            var project = await repository.ReadSingle(id);
+            var project = await UnitOfWork.Repository<Project>().GetByIdAsync(id);
 
             if (project == null)
                 return NotFound();
 
-            var airExchangeProject = repository.ReadAirExchange(id);
+            var airExchangeProject = CalculcateAirExchange(project.Id);//repository.ReadAirExchange(id);
 
             return Ok(airExchangeProject);
+        }
+
+        private AirExchangeProjectResource CalculcateAirExchange(int projectId) {            
+
+            var rooms = UnitOfWork.Repository<Room>().GetEnumerable(x => x.ProjectId == projectId);
+
+            var airExchangeRooms = new List<AirExchangeRoomResource>();
+
+            foreach (var room in rooms) {
+                var roomTypeValues = UnitOfWork.Repository<RoomTypeValue>().GetEnumerable(x => x.RoomTypeId == room.RoomTypeId).ToList(); //context.RoomTypeValues.Where(rtv => rtv.RoomTypeId == room.RoomTypeId).ToList();
+                int roomTypeValueId = 0;
+
+                //Площадь помещения
+                var roomArea = (room.Area != null) ? room.Area : room.Length * room.Width;
+                //Объем помещения
+                var roomVolume = (room.Area * room.Height != null) ? room.Area * room.Height : room.Length * room.Width * room.Height;
+                //Кол-во людей в помещении
+                var roomPeopleAmount = (room.PeopleAmount != null) ? room.PeopleAmount : 0;
+
+                //Инициализация ИД показателей типа помещения
+                if (roomTypeValues.Count == 1) {
+                    roomTypeValueId = roomTypeValues
+                        .Where(rtv => rtv.RoomTypeId == room.RoomTypeId)
+                        .SingleOrDefault().Id;
+                } else {
+                    //TODO: Заменить логической обработкой
+                    //TODO: Добавить "валидацию" по обязательному указанию площади и кол-ву людей для тех типов помещений,
+                    //которые их используют по условию, например "при общей площади квартиры на одного человека менее 20 м²"
+                    //Площадь на одного человека
+                    var roomPerPeopleArea = (roomArea != 0 && roomPeopleAmount != 0) ? roomArea / roomPeopleAmount : 0;
+                    switch (room.RoomTypeId) {
+                        case 8:
+                            roomTypeValueId = (roomPerPeopleArea < 20) ? 5 : 6;
+                            break;
+                        case 9:
+                            roomTypeValueId = (roomPerPeopleArea < 20) ? 7 : 8;
+                            break;
+                        case 15:
+                            roomTypeValueId = (roomPerPeopleArea < 20) ? 14 : 15;
+                            break;
+                        case 20:
+                            roomTypeValueId = (roomPerPeopleArea < 20) ? 20 : 21;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                var roomTypeValue = roomTypeValues.Where(rtv => rtv.Id == roomTypeValueId).SingleOrDefault();
+
+                //«Приток» по формуле: «Приток по жилой площади 1 м2, м3/ч» * «Площадь помещения»
+                var inflowArea = roomTypeValue.InflowArea * roomArea;
+                //«Вытяжка» по формуле: «Вытяжка по жилой площади 1 м2, м3/ч» * «Площадь помещения»
+                var exhaustArea = roomTypeValue.ExhaustArea * roomArea;
+
+                //«Приток» по формуле: «Приток по кратности 1/ч, крат» * «Объем помещения»
+                var inflowMultiply = roomTypeValue.InflowMultiply * roomVolume;
+                //«Вытяжка» по формуле: «Вытяжка по кратности 1/ч, крат» * «Объем помещения»
+                var exhaustMultiply = roomTypeValue.ExhaustMultiply * roomVolume;
+
+                //«Приток» по формуле: «Приток по людям на 1 человека, м3/ч» * «Кол-во людей в помещении»
+                var inflowPeople = roomTypeValue.InflowPeople * roomPeopleAmount;
+                //«Вытяжка» по формуле: «Вытяжка по людям на 1 человека, м3/ч» * «Кол-во людей в помещении»
+                var exhaustPeople = roomTypeValue.ExhaustPeople * roomPeopleAmount;
+
+                //«Приток» по константе
+                var inflow = roomTypeValue.Inflow;
+                //«Вытяжка» по константе
+                var exhaust = roomTypeValue.Exhaust;
+
+                //"Приток". Максимальное значение
+                double?[] inflowArray = {
+                    inflowArea,
+                    inflowPeople,
+                    inflowMultiply,
+                    inflow
+                };
+                var inflowMaxValue = inflowArray.Max();
+
+                //"Вытяжка". Максимальное значение
+                double?[] exhaustArray = {
+                    exhaustArea,
+                    exhaustPeople,
+                    exhaustMultiply,
+                    exhaust
+                };
+                var exhaustMaxValue = exhaustArray.Max();
+
+                airExchangeRooms.Add(new AirExchangeRoomResource() {
+
+                    Id = room.Id,
+                        RoomNumber = room.RoomNumber,
+                        RoomName = room.RoomName,
+                        TempIn = roomTypeValue.TempIn,
+                        Area = roomArea,
+                        Volume = roomVolume,
+                        PeopleAmount = room.PeopleAmount,
+                        InflowMultiply = roomTypeValue.InflowMultiply,
+                        ExhaustMultiply = roomTypeValue.ExhaustMultiply,
+                        InflowCalc = inflowMaxValue,
+                        ExhaustCalc = exhaustMaxValue
+                });
+            }
+
+            var airExchangeProject = new AirExchangeProjectResource() {
+                Id = projectId,
+                InflowTotal = airExchangeRooms.Sum(x => x.InflowCalc),
+                ExhaustTotal = airExchangeRooms.Sum(x => x.ExhaustCalc),
+                AirExchangeRooms = airExchangeRooms
+            };
+
+            return airExchangeProject;
         }
 
     }
--- angularApp/app/core/services/air-exchange-calculate.service.ts
+++ angularApp/app/core/services/air-exchange-calculate.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Configuration } from '../../app.constants';
 import { AirExchangeProject } from '../../models/airExchangeProject';
+import { TOKEN_NAME } from './user.service';
 
 @Injectable()
 export class AirExchangeCalculateService {
@@ -20,7 +21,13 @@ export class AirExchangeCalculateService {
 
   Get(id: number): Observable<AirExchangeProject> {
     console.log(this.actionUrl + id + '/airexchange');
+    let headers = this.headers;
+    headers = headers.append('Authorization', `Bearer ${this.getToken()}`);
     return this.http.get<AirExchangeProject>(this.actionUrl + id + '/airexchange', { headers: this.headers });
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(TOKEN_NAME);
   }
 
 }
