440f354e19196eb2a414da2e6270c25e8db505f5
ViktorBaklyshkin <icwee@icloud.com>
2019-02-09 22:08:28 +0300

    Добавлены Users

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -1,4 +1,7 @@
+using System.Linq;
+using System.Security.Claims;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using VentCalc.Models;
 using VentCalc.Persistence;
@@ -9,15 +12,28 @@ namespace VentCalc.Controllers {
 
         public readonly IMapper Mapper;
         public IUnitOfWork UnitOfWork;
-        public BaseController(IMapper mapper, IUnitOfWork uow)
-        {
+        protected readonly IHttpContextAccessor HttpContextAccessor;
+        protected readonly User CurrentUser;
+        public BaseController(IMapper mapper, IUnitOfWork uow) {
             this.Mapper = mapper;
             this.UnitOfWork = uow;
         }
 
-        public async System.Threading.Tasks.Task<int> GetCurrentUserIdAsync(string id){
-            var user = await UnitOfWork.Repository<PortalUser>().GetSingleIcludeMultipleAsync(x => x.IdentityId == id, x => x.Identity); 
+        public async System.Threading.Tasks.Task<int> GetCurrentUserIdAsync(string id) {
+            var user = await UnitOfWork.Repository<PortalUser>().GetSingleIcludeMultipleAsync(x => x.IdentityId == id, x => x.Identity);
             return user?.Id ?? 0;
+        }
+
+        protected BaseController(IMapper mapper, IUnitOfWork uow, IHttpContextAccessor httpContextAccessor) {
+            Mapper = mapper;
+            UnitOfWork = uow;
+            HttpContextAccessor = httpContextAccessor;
+            CurrentUser = GetCurrentUser();
+        }
+
+        private User GetCurrentUser() {
+            var login = HttpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return UnitOfWork.Repository<User>().GetEnumerable(x => x.Login == login).FirstOrDefault();
         }
 
     }
